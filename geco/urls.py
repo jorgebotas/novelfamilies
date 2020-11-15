7efb05fcd0ffc49f6dec6c1cfedfd4eecf09f5e9
Jorge Botas <jorgebotasmiret@protonmail.com>
2020-11-15 21:20:49 +0100

    geco

--- geco/urls.py
+++ geco/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     path('', views.context, name='context'),
     path(r'api/newick/<str:query>/', api.newick),
     path(r'api/context/<str:query>/<int:cutoff>/', api.context),
-
 ]
 
