f07087c298beb6c42a6a4400e4f381d0779a082e
Jorge Botas <jorgebotasmiret@protonmail.com>
2020-11-15 23:22:22 +0100

    geco

--- geco/static/geco/js/gmgc.js
+++ geco/static/geco/js/gmgc.js
@@ -43,7 +43,6 @@ var gmgc_vueapp = new Vue({
                 "nnodes": 49,
                 "taxscope": "Bilateria",
                 "desc": " Some description 1",
-                "context" : '{"0": {"neighborhood": {"0": {"preferred_name": "rpsH", "strand": "-"}, "1": {"preferred_name": "rplE", "strand": "-"}, "2": {"preferred_name": "rplX", "strand": "-"}, "3": {"preferred_name": "rplN", "strand": "-"}, "4": {"preferred_name": "rpsQ", "strand": "-"}, "5": {"preferred_name": "rpmC", "strand": "-"}, "6": {"preferred_name": "rplP", "strand": "-"}, "7": {"preferred_name": "rpsC", "strand": "-"}, "8": {"preferred_name": "rplV", "strand": "-"}, "9": {"preferred_name": "rpsS", "strand": "-"}, "10": {"preferred_name": "rplB", "strand": "-"}, "-1": {"preferred_name": "rplF", "strand": "-"}, "-10": {"preferred_name": "rpoA", "strand": "-"}, "-9": {"preferred_name": "rpsK", "strand": "-"}, "-8": {"preferred_name": "rpsM", "strand": "-"}, "-7": {"preferred_name": "rpmJ", "strand": "-"}, "-6": {"preferred_name": "none", "strand": "-"}, "-5": {"preferred_name": "rplO", "strand": "-"}, "-4": {"preferred_name": "rpmD", "strand": "-"}, "-3": {"preferred_name": "rpsE", "strand": "-"}, "-2": {"preferred_name": "rplR", "strand": "-"}}}, "1": {"neighborhood": {"0": {"preferred_name": "rpsH", "strand": "-"}, "1": {"preferred_name": "rpsZ", "strand": "-"}, "2": {"preferred_name": "rplE", "strand": "-"}, "3": {"preferred_name": "rplX", "strand": "-"}, "4": {"preferred_name": "rplN", "strand": "-"}, "5": {"preferred_name": "rpsQ", "strand": "-"}, "6": {"preferred_name": "rpmC", "strand": "-"}, "7": {"preferred_name": "rplP", "strand": "-"}, "8": {"preferred_name": "rpsC", "strand": "-"}, "9": {"preferred_name": "rplV", "strand": "-"}, "10": {"preferred_name": "rpsS", "strand": "-"}, "-1": {"preferred_name": "rplF", "strand": "-"}, "-10": {"preferred_name": "none", "strand": "-"}, "-9": {"preferred_name": "none", "strand": "-"}, "-8": {"preferred_name": "map", "strand": "-"}, "-7": {"preferred_name": "adk", "strand": "-"}, "-6": {"preferred_name": "secY", "strand": "-"}, "-5": {"preferred_name": "rplO", "strand": "-"}, "-4": {"preferred_name": "rpmD", "strand": "-"}, "-3": {"preferred_name": "rpsE", "strand": "-"}, "-2": {"preferred_name": "rplR", "strand": "-"}}}}',
             },
 
             "tree_name2": {
@@ -90,19 +89,17 @@ var gmgc_vueapp = new Vue({
         },
 
         toggleGeCo : async function(selector, query) {
-            console.log(query)
             let newick = this.show_items[query].newick;
             let context = this.show_items[query].context;
-            console.log(context)
+            let colors = await get_colors();
             if (context) {
                 await window.launch_GeCo(selector, context, newick, 41, colors);
             } else {
                 query = "095_560_840";
                 await $(selector + " .geco-loader").show();
-                let newick = await get_newick(query);
-                let context = await get_context(query, "cluster", 30);
+                newick = await get_newick(query);
+                context = await get_context(query, "cluster", 30);
                 console.log(context);
-                let colors = await get_colors();
                 await window.launch_GeCo(selector, context, newick, 41, colors);
                 await $(selector + " .geco-loader").hide();
                 this.show_items[query].newick = newick;
