d96082142661e2d5e7d2a2ea1ae740e641d46f24
Jorge Botas <jorgebotasmiret@protonmail.com>
2020-11-15 20:04:27 +0100

    api

--- geco/urls.py
+++ geco/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+from . import api
 from . import views
 
 urlpatterns = [
-    path('', views.context, name='context')
+    path('', views.context, name='context'),
     path(r'api/newick/<str:query>/', api.newick),
     path(r'api/context/<str:query>/<int:cutoff>/', api.context),
 
