15711871af1c15f54be335398a6a38fb89fd4889
Jorge Botas <jorgebotasmiret@protonmail.com>
2020-11-17 19:23:06 +0100

    mongo

--- geco/src/get_context.py
+++ geco/src/get_context.py
@@ -736,11 +736,11 @@ def get_context(query, n_range, cutoff, cluster=True, isList=False, json=False):
 
     ### MongoDB call
     # global client, db, coll_unigenes, coll_clusters, coll_e5
-    # client,\
-    # db,\
-    # coll_unigenes,\
-    # coll_clusters,\
-    # coll_e5 = mongo_connect()
+    client,\
+    db,\
+    coll_unigenes,\
+    coll_clusters,\
+    coll_e5 = mongo_connect()
 
     # global ncbi
     ncbi = NCBITaxa()
