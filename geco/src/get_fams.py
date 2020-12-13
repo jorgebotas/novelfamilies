c60317460db05a75758426637986648e99f3e61e
Jorge Botas <jorgebotasmiret@protonmail.com>
2020-12-13 14:18:15 +0100

    db

--- geco/src/get_fams.py
+++ geco/src/get_fams.py
@@ -3,7 +3,7 @@ from .mongodb import mongo_connect_novelfams
 def get_fam_info(identifier):
     gf = mongo_connect_novelfams()[1]
     df_identif = int(identifier.replace("_", ""))
-    rawd = gf.find({'gf' : df_identif})
+    rawd = gf.find({'gf' : df_identif})[0]
     data = {
         "name" : rawd.gfn,
         "members" : rawd.unigenes
