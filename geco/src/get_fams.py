82c74b09ea56c3bad42e346c1d68ee3c68da8e38
Jorge Botas <jorgebotasmiret@protonmail.com>
2020-12-13 14:17:26 +0100

    db

--- geco/src/get_fams.py
+++ geco/src/get_fams.py
@@ -3,10 +3,9 @@ from .mongodb import mongo_connect_novelfams
 def get_fam_info(identifier):
     gf = mongo_connect_novelfams()[1]
     df_identif = int(identifier.replace("_", ""))
-    print(df_identif)
-    data = gf.find({'gf' : df_identif})
-    print(data)
-    for r in data:
-        print(r)
-        data = r
-    return dict(data)
+    rawd = gf.find({'gf' : df_identif})
+    data = {
+        "name" : rawd.gfn,
+        "members" : rawd.unigenes
+    }
+    return data
