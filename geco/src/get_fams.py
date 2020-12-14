3d1f347d0b3e6f3be962bf2a6a4edab008545cde
Jorge Botas <jorgebotasmiret@protonmail.com>
2020-12-14 14:41:28 +0100

    theme

--- geco/src/get_fams.py
+++ geco/src/get_fams.py
@@ -2,12 +2,21 @@ from .mongodb import mongo_connect_novelfams
 
 def get_fam_info(identifier):
     gf = mongo_connect_novelfams()[1]
-    df_identif = int(identifier.replace("_", ""))
-    rawd = gf.find({'gf' : df_identif})[0]
+    int_identif = int(identifier.replace("_", ""))
+    rawd = gf.find({'gf' : int_identif})[0]
     data = {
         "name" : rawd['gf'],
         "members" : rawd['unigenes'],
         "keggp" : rawd['keggp'],
         'tapx' : rawd['taxp']
     }
+    get_neighborhood
     return data
+
+def get_neighborhood(identifier):
+    gmgc_neigh = mongo_connect()[2]
+    int_identif = int(identifier.replace("_", ""))
+    rawd = gmgc_neigh.fing({'gf' : int_identif})[0]['neigh']
+    for n in rawd:
+        print("\n\n")
+        print(n)
