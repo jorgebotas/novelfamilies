1e2f86e0f80b10832f8ca8768c6f322ba0e925ab
Jorge Botas <jorgebotasmiret@protonmail.com>
2020-12-27 19:00:08 +0100

    preds

--- geco/src/get_fams.py
+++ geco/src/get_fams.py
@@ -26,8 +26,6 @@ def get_fam_info(identifier):
         'domains' : domains,
         'biomes' : rawd['biomep']
     }
-    print(rawd['p_keggp'])
-    # print(rawd['p_cogp'])
     return data
 
 def get_neighborhood(identifier):
