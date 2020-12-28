03179b2866d8f356e27aeca2e13f6006326e732f
Jorge Botas <jorgebotasmiret@protonmail.com>
2020-12-28 12:38:16 +0100

    preds

--- geco/src/get_fams.py
+++ geco/src/get_fams.py
@@ -3,34 +3,42 @@ from .mongodb import mongo_connect_novelfams
 def toJSON(l, identifier):
     output = []
     for item in l:
-        # item = eval(item)
         k = list(item.keys())[0]
         d = list(item.values())[0]
         output.append({ identifier : k, **d})
     return output
 
 def get_fam_info(identifier):
-    gf = mongo_connect_novelfams()[1]
-    int_identif = int(identifier.replace("_", ""))
-    rawd = gf.find({'gf' : int_identif})[0]
-    doms = rawd['domains']
+    # Connect to MongoDB
+    db,
+        gf,
+        gmgcv1_gf,
+        gmgcv1_neighs = mongo_connect_novelfams()
+    # int_identif = int(identifier.replace("_", ""))
+    gf_data = gf.find({'gfn' : identifier})[0]
+    gmgcv1_data = gmgcv1_gf.find({'gf' : gf_data['gf']})[0]
+    # Format domains info
+    doms = gmgcv1_data['domains']
     domains = {}
     for i in range(len(doms)):
         d = eval(doms[i])
         domains[list(d.keys())[0]] = list(d.values())[0]
     data = {
-        "name" : rawd['gf'],
-        "members" : rawd['unigenes'].split(","),
-        'keggp' :  toJSON([eval(k) for k  in rawd['p_keggp']], 'kegg'),
-        'cogp' :  toJSON(rawd['p_cogp'], 'cog'),
+        "name" : gmgcv1_data['gf'],
+        "members" : gmgcv1_data['unigenes'].split(","),
+        'keggp' :  toJSON([eval(k) for k  in gmgcv1_data['p_keggp']], 'kegg'),
+        'cogp' :  toJSON(gmgcv1_data['p_cogp'], 'cog'),
         'domains' : domains,
-        'biomes' : rawd['biomep'],
-        'taxp' :  [eval(i) for i in rawd['p_taxp']],
+        'biomes' : gmgcv1_data['biomep'],
+        'taxp' :  [eval(i) for i in gmgcv1_data['p_taxp']],
     }
-    print(rawd['p_taxp'])
+    print(gf_data['source'])
+    print(gf_data['ftype'])
+    print(gf_data['hom'])
+    print(gf_data['flength'])
     return data
 
 def get_neighborhood(identifier):
     gmgc_neigh = mongo_connect_novelfams()[2]
     int_identif = int(identifier.replace("_", ""))
-    rawd = gmgc_neigh.find({'gf' : int_identif})[0]['neigh']
+    gmgcv1_data = gmgc_neigh.find({'gf' : int_identif})[0]['neigh']
--- geco/src/mongodb.py
+++ geco/src/mongodb.py
@@ -17,6 +17,7 @@ def mongo_connect():
 def mongo_connect_novelfams():
     client = MongoClient('10.0.3.1', 27017, maxPoolSize=10)
     db = client.novel_fam
-    gf = db.gf_profile_gmgcv1
+    gf = db.gene_families
+    gmgcv1_gf = db.gf_profile_gmgcv1
     gmgcv1_neighs = db.gmgcv1_neighs
-    return db, gf, gmgcv1_neighs
+    return db, gf, gmgcv1_gf, gmgcv1_neighs
