fc1590812ea31563b53769f0142543f1acbc42a5
Jorge Botas <jorgebotasmiret@protonmail.com>
2021-01-07 17:40:54 +0100

    pred

--- geco/src/get_fams.py
+++ geco/src/get_fams.py
@@ -67,10 +67,11 @@ def get_neighborhood(identifier, origin):
     # gf = gf[:3] + "_" + gf[3:6] + "_" + gf[6:]
     try:
         identifier = int(identifier)
-        gf = int(get_gf(identifier))
+        gfn = int(get_gf(identifier))
+        search = {'gfn' : gfn}
     except:
         gf = int(str(identifier).replace("_", ""))
-    search = {'gf' : gf}
+        search = {'gf' : gf}
     print(gf)
     if origin == "gmgc":
         data = gmgcv1_neighs.find(search)[0]['neigh']
