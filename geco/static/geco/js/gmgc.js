5066e764f511e185579c623b8b70c2c43f2c8f75
Jorge Botas <jorgebotasmiret@protonmail.com>
2021-01-09 22:07:13 +0100

    seqface

--- geco/static/geco/js/gmgc.js
+++ geco/static/geco/js/gmgc.js
@@ -162,11 +162,11 @@ var renderDonut = function(id, labels, vals, colors) {
 var renderDomains = function(domains) {
         palette = { 'helix' : '#e6ac00',
                     'sp' : '#6574cd'}
-        //document.querySelectorAll('.domains').forEach(div => {
-            //if (div.children.length > 0) {
-                //div.firstChild.remove();
-            //}
-        //})
+        document.querySelectorAll('.domains').forEach(div => {
+            if (div.children.length > 0) {
+                div.firstChild.remove();
+            }
+        })
         domains.forEach(d => {
             selector = "d" + d.gene
             draw_protDomains(selector, d.doms, 1000, 600, 10, palette);
