4509b595cd4309b0e53612a31d61d60202aa4d72
Jorge Botas <jorgebotasmiret@protonmail.com>
2021-01-09 16:02:17 +0100

    seqface

--- geco/static/geco/js/gmgc.js
+++ geco/static/geco/js/gmgc.js
@@ -28,6 +28,34 @@ var get_colors = async () => {
     return colors;
 }
 
+var draw_protDomains = function(id, lenseq, domains, width, height) {
+    function scale(num, inSize, outSize) {
+        return +num * outSize / inSize;
+    }
+    function draw_seqLine(g, width, height) {
+        g.append("line")
+            .attr("stroke", "black")
+            .attr("stroke-width", 2)
+            .attr("x1", 0)
+            .attr("y1", height / 2)
+            .attr("x2", width)
+            .attr("y2", height / 2);
+    }
+    function draw_domains(g, lenseq, domains, width, height) {
+        g.selectAll('rect')
+            .data(domains)
+            .enter().append('rect')
+            .attr("x", function (d) { return scale(d.start, lenseq, width); })
+            .attr("y", 0)
+            .attr("width", function (d) { return scale(+d.end - +d.start, lenseq, width); })
+            .attr("height", height)
+            .attr("fill", "orange");
+    }
+    var g = d3.select('#' + id).append('svg:svg');
+    draw_seqLine(g, width, height);
+    draw_domains(g, lenseq, domains, width, height);
+}
+
 var drawDonuts = async function(f, data) {
     var biomes_id = 'f' + f + '-biomesViz';
     var biomes = data.biomes;
