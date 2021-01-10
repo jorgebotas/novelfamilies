b2543f5c5f4189edd557fdbb79a88e23c89ea686
Jorge Botas <jorgebotasmiret@protonmail.com>
2021-01-10 01:41:46 +0100

    domains

--- geco/static/geco/js/gmgc.js
+++ geco/static/geco/js/gmgc.js
@@ -1,8 +1,6 @@
 API_BASE_URL = "/api"
 STATIC_URL = "/static/geco"
 
-import draw_protDomains from './domains.js'
-
 var get_newick = async (query) => {
     let newick;
     await fetch(API_BASE_URL + '/newick/' + query + '/')
@@ -30,6 +28,74 @@ var get_colors = async () => {
     return colors;
 }
 
+var draw_protDomains = function(id, domains, lenseq, width, height, palette) {
+    function scale(num, inSize, outSize) {
+        return +num * outSize / inSize;
+    }
+    function draw_seqLine(g, width, height) {
+        g.append("line")
+            .attr("stroke", "black")
+            .attr("stroke-width", 2)
+            .attr("x1", 0)
+            .attr("y1", height / 2)
+            .attr("x2", width)
+            .attr("y2", height / 2);
+    }
+    function draw_legend(selector, domains, palette) {
+        var legend = d3.select(selector)
+         .append("div")
+         .attr("class", "d-inline px-3");
+        var doms = new Set();
+        domains.forEach(d => {
+            if (d.class && d.class != "") {
+                doms.add(d.class)
+            }
+        })
+        doms.forEach(d => {
+            let l = legend.append("div")
+                     .attr('class', 'd-inline px-2');
+            l.append('svg')
+             .attr('width', 10)
+             .attr('height', 10)
+             .attr('class', 'mr-2')
+             .append('circle')
+             .attr("r", 5)
+             .attr("cx", 5)
+             .attr("cy", 5)
+             .attr("fill", palette(d));
+            l.append('div')
+             .attr('class', 'd-inline')
+             .text(d)
+        })
+    }
+    function draw_domains(g, domains, lenseq, width, height, palette) {
+        g.selectAll('circle')
+            .data(domains.filter(d => d.shape == "circle" ))
+            .enter().append('circle')
+            .attr("r", 4)
+            .attr("cx", function (d) { return scale(+d.c, lenseq, width); })
+            .attr("cy", height/2)
+            .attr("fill", d => { return palette(d.class) });
+        g.selectAll('rect')
+            .data(domains.filter(d => d.shape == "rect" ))
+            .enter().append('rect')
+            .attr("x", function (d) {return scale(+d.start, lenseq, width); })
+            .attr("y", 0)
+            .attr("width", function (d) { return scale(+d.end - +d.start, lenseq, width); })
+            .attr("height", height)
+            .attr("fill", d => { return palette(d.class) });
+    }
+    var g = d3.select('#' + id)
+              .append("div")
+              .append('svg:svg')
+              .attr("width", width)
+              .attr("height", height)
+              .append('svg:g')
+                .attr("transform", "translate(" + 5 + ", 0)");
+    draw_seqLine(g, width, height);
+    draw_domains(g, domains, lenseq, width, height, palette);
+    draw_legend('#' + id + ' div', domains, palette);
+}
 
 var drawDonuts = async function(f, data) {
     var biomes_id = 'f' + f + '-biomesViz';
