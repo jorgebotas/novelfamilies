e9ab84dd0496285a88335717717e78d97e98aded
Jorge Botas <jorgebotasmiret@protonmail.com>
2021-01-12 01:32:39 +0100

    domains

--- geco/src/get_fams.py
+++ geco/src/get_fams.py
@@ -124,7 +124,8 @@ def get_neighborhood(identifier, origin):
     elif origin == "tara":
         data = tara_mags_neighs.find(search)[0]['neigh']
     elif origin == "earth":
-        print(earth_mags_neighs.find({search}))
+        for e in earth_mags_neighs.find(search):
+            print(e)
         data = earth_mags_neighs.find(search)[0]['neigh']
     else:
         data = {}
