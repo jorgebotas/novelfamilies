9b656176c2f0b3bf15401aadc3e08c5548c5dd40
Jorge Botas <jorgebotasmiret@protonmail.com>
2021-01-14 15:51:36 +0100

    domains

--- geco/src/get_fams.py
+++ geco/src/get_fams.py
@@ -115,6 +115,7 @@ def get_fam_info(identifier, is_gf=True):
         'p_exp' : gf_data['p_exp'],
         'align' : gf_data['algstats'],
     }
+    print(mags_annot)
     return data
 
 def get_neighborhood(identifier, origin):
