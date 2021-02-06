ed3dc320587d95fde5ad925432b09d6205a296b2
Jorge Botas <jorgebotasmiret@protonmail.com>
2021-02-06 12:09:00 +0100

    code

--- geco/src/get_fams.py
+++ geco/src/get_fams.py
@@ -136,7 +136,6 @@ def get_fam_info(identifier, is_gf=True):
         'p_exp' : gf_data['p_exp'],
         'align' : gf_data['algstats'],
     }
-    print(data['keggp'])
     return data
 
 def get_neighborhood(identifier, origin):
