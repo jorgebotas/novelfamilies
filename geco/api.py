2856cc1490256ac3c7286c5b5cd94a352b894b5b
Jorge Botas <jorgebotasmiret@protonmail.com>
2021-03-11 13:12:33 +0100

    slider

--- geco/api.py
+++ geco/api.py
@@ -60,6 +60,7 @@ def fam_by_taxa(request, query, spec, cov):
     """Return list with fams that match taxa search
     """
     fams = fams_by_taxa(query, float(spec), float(cov))
+    print(fams)
     fams = { 'show_items' : fams }
     return JsonResponse(fams)
 
