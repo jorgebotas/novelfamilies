c0b1c7114445ac1a1dd7c32fead2db2a720a5a3b
Jorge Botas <jorgebotasmiret@protonmail.com>
2021-04-09 16:51:28 +0200

    pagination

--- geco/src/query_fam.py
+++ geco/src/query_fam.py
@@ -329,7 +329,7 @@ def get_more_faminfo(fams):
             tax = get_taxonomy(genome, json=False)
             taxonomy.append(tax)
         ext_fam['domains'] = domains
-        tax_counter = list(zip(Counter(taxonomy)))
+        tax_counter = Counter(taxonomy)
         print(tax_counter)
         ext_fam['taxonomy'] = tax_counter
         extended_fams.append(ext_fam)
