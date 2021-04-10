33f370ad64ab3016174bea063d5d9cf7a5f866c0
Jorge Botas <jorgebotasmiret@protonmail.com>
2021-04-10 14:48:31 +0200

    summary

--- geco/src/query_fam.py
+++ geco/src/query_fam.py
@@ -51,8 +51,7 @@ def get_taxonomy(genome, json=True):
     taxa = match['lineage'].split(';')
     parsed_taxa = []
     for t in taxa:
-        print(t)
-        if t.split('_')[-1] == '_':
+        if t[-1] == '_':
             print(t)
             continue
         parsed_taxa.append(t)
