d9cf2b668b34f20414c5b3611a02b1392a40eada
Jorge Botas <jorgebotasmiret@protonmail.com>
2021-04-12 01:00:33 +0200

    Sunburst

--- geco/static/geco/js/SeqSunburst.js
+++ geco/static/geco/js/SeqSunburst.js
@@ -235,7 +235,7 @@ var capitalize = function(string) {
 class BreadCrumb {
     constructor(selector, palette, fields, seq, options = { showFields: true }) {
         // Polygon dimensions
-        this.polygonWidth = 150;
+        this.polygonWidth = 120;
         this.polygonHeight = 30;
         this.polygonPadding = 2;
         this.tipWidth = 10;
