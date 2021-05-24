0c422bc7f7202d37e0f474e8793507f73c7efb3b
jorgebotas <jorgebotasmiret@protonmail.com>
2021-05-24 12:10:14 +0200

    CARD examples

--- nmgfams_app/src/query_fam.py
+++ nmgfams_app/src/query_fam.py
@@ -351,7 +351,7 @@ def get_neighborhood_summary(fam):
                 most_conserved_og['description'] = \
                         f'Level: {level}. {og["description"]}'
         most_conserved_og['level'] = 'most conserved'
-        s['Orthologous groups'].insert(most_conserved_og, 0)
+        s['Orthologous groups'].insert(0, most_conserved_og)
 
     return summary
 
