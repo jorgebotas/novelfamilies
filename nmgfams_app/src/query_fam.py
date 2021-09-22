fa833276e6314d2059a4a93b5272addf6bfc63ab
jorgebotas <jorgebotasmiret@protonmail.com>
2021-09-22 11:55:32 +0200

    intro

--- nmgfams_app/src/query_fam.py
+++ nmgfams_app/src/query_fam.py
@@ -454,7 +454,7 @@ def get_more_faminfo(fams):
             genome =  m.split('@')[1]
         ext_fam['domains'] = domains
         biome = [ { "biome": k, "count": v } for k, v in habitat.get(fname, {}).items()]
-        ext_fam["biome"] = sorted(biome, key=lambda d: d["count"], reverse=True)
+        ext_fam["biome"] = sorted(biome, key=lambda d: int(d["count"]), reverse=True)
         print(ext_fam["biome"])
         unique_genomes = list(set(m.split('@')[1] for m in fam['members']))
         taxonomy = [get_taxonomy(g, json=False)
