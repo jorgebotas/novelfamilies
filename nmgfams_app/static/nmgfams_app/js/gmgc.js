0b97d869b692bb96aa46304f3309a610628c0aa2
jorgebotas <jorgebotasmiret@protonmail.com>
2021-09-29 11:15:28 +0200

    intro

--- nmgfams_app/static/nmgfams_app/js/gmgc.js
+++ nmgfams_app/static/nmgfams_app/js/gmgc.js
@@ -524,7 +524,7 @@ var gmgc_vueapp = new Vue({
 
                 // Render sunbursts
                 const biomeSunburstSelector = `#f${idx}-biomeSunburst`
-                SeqSunburst(data.biome, 400, ":")
+                SeqSunburst(data.biome, 600, ":")
                     .draw(biomeSunburstSelector);
             });
         },
