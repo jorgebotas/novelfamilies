4d53f7933656e267e47f6efea91ac159e91cbd5e
jorgebotas <jorgebotasmiret@protonmail.com>
2021-09-29 11:26:41 +0200

    intro

--- nmgfams_app/static/nmgfams_app/js/SeqSunburst.js
+++ nmgfams_app/static/nmgfams_app/js/SeqSunburst.js
@@ -206,7 +206,7 @@ var SeqSunburst = function(unformattedData, width, separator=";") {
 
         function labelTransform(d) {
           const x = (d.x0 + d.x1) / 2 * 180 / Math.PI;
-          const y = (d.y0 + d.y1) / 2 * radius;
+          const y = (d.y0 + d.y1) / (2 * radius);
           return `rotate(${x - 90}) translate(${y},0) rotate(${x < 180 ? 0 : 180})`;
         }
 
